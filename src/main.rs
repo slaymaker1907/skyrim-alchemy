9e80d84d063c146928e08704cfb09abb611eda7b
slaymaker1907 <dyllongagnier@gmail.com>
2018-01-16 12:53:11 -0700

    Optimized by ignoring obvious uniform distributions.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     contras.insert(EntropyConstraint::SingleNeq(VarAndValue{var: 1, value: 1}));
     contras.insert(EntropyConstraint::DoubleNeq(0, 1));
     contras.insert(EntropyConstraint::DoubleNeq(1, 2));
-    let optimizer = EntropyOptimizer{ varc:25, k: 25, contras};
+    let optimizer = EntropyOptimizer{ varc:100, k: 100, contras};
     let best = optimizer.optimize();
     // println!("{}", best);
     println!("{}", best.entropy());
